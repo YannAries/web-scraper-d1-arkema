9110e7207371bcb9974989a85551b3b48f621d1e
YannAries <YannAries@users.noreply.github.com>
2024-03-02 01:53:44 -0500

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+const ObjectsToCsv = require('objects-to-csv');
 
 (async () => {
     let browser;
     try {
         browser = await puppeteer.launch({
-            headless: true,
+            headless: 'shell',
             args: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
         });
 
         const page = await browser.newPage();
-
         await page.setViewport({ width: 1280, height: 800 });
 
-        // Go to the FFF webpage and set a timeout
-        await page.goto('https://www.fff.fr/competition/engagement/394572-d1-arkema/phase/1/classement.html', { waitUntil: 'networkidle2', timeout: 0 });
-
-        // Optimize the wait times here or add improvements in the page.$$eval commands
+        await page.goto('https://www.fff.fr/competition/engagement/407655-d1-arkema/phase/1/classement.html', { waitUntil: 'networkidle2', timeout: 0 });
+        /*         await page.goto('https://www.fff.fr/competition/engagement/394572-d1-arkema/phase/1/classement.html', { waitUntil: 'networkidle2', timeout: 0 });
+         */
         const d1ArkemaTable = await page.$$eval("table > tbody > tr", rows => {
             return rows.map(row => {
-                const cells = [...row.cells].map(cell => cell.innerText);
+                const cells = Array.from(row.cells).map(cell => cell.innerText);
                 return {
                     team: cells[2],
                     points: cells[3],
@@ -34,21 +35,32 @@ const puppeteer = require('puppeteer');
             });
         });
 
-        // Screenshot of the FFF webpage
-        await page.screenshot({ path: './dist/assets/image/screenshot.png', fullPage: true });
+        // Debug
+        /* console.log(d1ArkemaTable); */
 
-        // Export data collected as CSV
-        const ObjectsToCsv = require('objects-to-csv');
+        // Ensure the directory exists
+        const screenshotDir = './dist/assets/image';
+        const screenshotPath = path.join(screenshotDir, 'screenshot.png');
+        ensureDirSync(screenshotDir);
+
+        await page.screenshot({ path: screenshotPath, fullPage: true });
+
         const csv = new ObjectsToCsv(d1ArkemaTable);
-        await csv.toDisk('./dist/assets/d1ArkemaData.csv');
+        await csv.toDisk('./dist/assets/d1ArkemaData.csv', { bom: true });
 
-        console.log('Tout est parfait !');
+
+        console.log('Data extraction and saving completed successfully.');
     } catch (err) {
         console.error(`Failed to capture screenshot & data: ${err}`);
     } finally {
-        // Close the browser at the end of the operation
         if (browser) {
             await browser.close();
         }
     }
 })();
+
+function ensureDirSync(dirPath) {
+    if (!fs.existsSync(dirPath)) {
+        fs.mkdirSync(dirPath, { recursive: true });
+    }
+}
